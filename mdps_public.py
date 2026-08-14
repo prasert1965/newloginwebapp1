fef67b0479067e7141ffece1edc6a215c26f5346
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-08-14 16:09:15 +0700

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -38,8 +38,7 @@ def main():
 	choice = st.sidebar.selectbox("Menu",menu)
 
 	if choice == "Home":
-		st.subheader("Home")
-	    st.subheader("This is Web app for prediction alpha thalassemia carriers")
+		st.subheader("Home This is Web app for prediction alpha thalassemia carriers")
 	
 	elif choice == "Login":
 		st.subheader("Login Section")
