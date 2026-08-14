2fcbf5d537e3396473d8c04d74e6e59b84a5832c
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-08-14 14:13:58 +0700

    Refactor user management and login functionality

--- mdps_public.py
+++ mdps_public.py
@@ -17,7 +17,41 @@ def login_user(username,password):
 import streamlit as st
 import pandas as pd
 
+elif choice == "SignUp":
+       st.subheader("Create an Account")
+	new_user = st.text_input('Username')
+	new_passwd = st.text_input('Password',type='password')
+	if st.button('SignUp'):
+		create_usertable()
+		add_userdata(new_user,make_hashes(new_passwd))
+		st.success("You have successfully created an account.Go to the Login Menu to login")
+		
+import hashlib
+def make_hashes(password):
+	return hashlib.sha256(str.encode(password)).hexdigest()
 
+def check_hashes(password,hashed_text):
+	if make_hashes(password) == hashed_text:
+		return hashed_text
+	return False
+
+# You can also use the verify functions of the various libraries for the same purpose
+
+elif choice == "Login":
+		user = st.sidebar.text_input('Username')
+		passwd = st.sidebar.text_input('Password',type='password')
+		if st.sidebar.checkbox('Login') :
+			create_usertable()
+			hashed_pswd = make_hashes(passwd)
+			result = login_user(user,check_hashes(passwd,hashed_pswd))
+			if result:
+				st.success("Logged In as {}".format(user))
+
+				# Tasks For Only Logged In Users
+				task = st.selectbox('Select Task',['Add Posts','Manage Blog','Profile'])
+				if task == "Add Posts":
+					st.subheader("Add Articles")
+					....
 # Security
 #passlib,hashlib,bcrypt,scrypt
 import hashlib
