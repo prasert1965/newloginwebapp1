aaac0a9ed82c559a7460fa19bf3dcc151f6b806a
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-08-14 16:06:45 +0700

    Refactor DB management and user authentication logic

--- mdps_public.py
+++ mdps_public.py
@@ -39,7 +39,7 @@ def main():
 
 	if choice == "Home":
 		st.subheader("Home")
-	    st.write("This is Web app for prediction alpha thalassemia carriers")
+	    st.subheader("This is Web app for prediction alpha thalassemia carriers")
 	elif choice == "Login":
 		st.subheader("Login Section")
 		
