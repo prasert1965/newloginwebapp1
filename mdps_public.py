5e864b203989d003c1806f6c988e70fce2241f72
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-08-14 15:27:19 +0700

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -14,18 +14,7 @@ def login_user(username,password):
 	c.execute('SELECT * FROM userstable WHERE username =? AND password = ?',(username,password))
 	data = c.fetchall()
 	return data
-
-	elif choice == "SignUp":
-		st.subheader("Create an Account")
-
-		new_user = st.text_input('Username')
-		new_passwd = st.text_input('Password',type='password')
-
-	if st.button('SignUp'):
-		create_usertable()
-		add_userdata(new_user,make_hashes(new_passwd))
-		st.success("You have successfully created an account.Go to the Login Menu to login")
- 
+	 
 import hashlib
 def make_hashes(password):
 	return hashlib.sha256(str.encode(password)).hexdigest()
