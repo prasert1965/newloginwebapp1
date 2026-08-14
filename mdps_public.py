af8c2543026c3fb2762f258bbbaa927fc6b90149
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-08-14 16:13:18 +0700

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -39,7 +39,8 @@ def main():
 
 	if choice == "Home":
 		st.subheader("This is Web app for prediction alpha thalassemia carriers")
-	
+		st.write('In 2024, From Phrae Adaboost model on Dataset3 demonstrated acc 97% sen 100% spec 95% AUC 0.974. In 2026, A performance evaluation of the adaboost model in the Khon Kaen population showed acc 63% Sen 93.4% Spec 57.4% PPV 30.9% NPV 97.7% AUC 0.754.')
+
 	elif choice == "Login":
 		st.subheader("Login Section")
 		
