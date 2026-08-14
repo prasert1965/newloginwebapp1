05d840560447ce6dbfac1fc28750271b7842c25b
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-08-14 14:36:22 +0700

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -1,19 +1,19 @@
 # DB Management
-import sqlite3 
-conn = sqlite3.connect('data.db')
-c = conn.cursor()
-def create_usertable():
-	c.execute('CREATE TABLE IF NOT EXISTS userstable(username TEXT,password TEXT)')
+#import sqlite3 
+#conn = sqlite3.connect('data.db')
+#c = conn.cursor()
+#def create_usertable():
+#	c.execute('CREATE TABLE IF NOT EXISTS userstable(username TEXT,password TEXT)')
 
 
-def add_userdata(username,password):
-	c.execute('INSERT INTO userstable(username,password) VALUES (?,?)',(username,password))
-	conn.commit()
+#def add_userdata(username,password):
+#	c.execute('INSERT INTO userstable(username,password) VALUES (?,?)',(username,password))
+#	conn.commit()
 
-def login_user(username,password):
-	c.execute('SELECT * FROM userstable WHERE username =? AND password = ?',(username,password))
-	data = c.fetchall()
-	return data
+#def login_user(username,password):
+#	c.execute('SELECT * FROM userstable WHERE username =? AND password = ?',(username,password))
+#	data = c.fetchall()
+#	return data
 import streamlit as st
 import pandas as pd
 
