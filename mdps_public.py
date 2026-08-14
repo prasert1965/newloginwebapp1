d2bb42fc0f43d6e3cf76f88a19eefec4904d906a
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-08-14 14:40:59 +0700

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -1,94 +1,5 @@
-# DB Management
-#import sqlite3 
-#conn = sqlite3.connect('data.db')
-#c = conn.cursor()
-#def create_usertable():
-#	c.execute('CREATE TABLE IF NOT EXISTS userstable(username TEXT,password TEXT)')
-
-
-#def add_userdata(username,password):
-#	c.execute('INSERT INTO userstable(username,password) VALUES (?,?)',(username,password))
-#	conn.commit()
-
-#def login_user(username,password):
-#	c.execute('SELECT * FROM userstable WHERE username =? AND password = ?',(username,password))
-#	data = c.fetchall()
-#	return data
 import streamlit as st
 import pandas as pd
-
-#elif choice == "SignUp":
-  #     st.subheader("Create an Account")
-#	new_user = st.text_input('Username')
-#	new_passwd = st.text_input('Password',type='password')
-#	if st.button('SignUp'):
-#		create_usertable()
-#		add_userdata(new_user,make_hashes(new_passwd))
-#		st.success("You have successfully created an account.Go to the Login Menu to login")
-		
-# import hashlib
-# def make_hashes(password):
-#	return hashlib.sha256(str.encode(password)).hexdigest()
-
-#def check_hashes(password,hashed_text):
-#	if make_hashes(password) == hashed_text:
-#		return hashed_text
-#	return False
-
-# You can also use the verify functions of the various libraries for the same purpose
-
-#        elif choice == "Login":
-#		user = st.sidebar.text_input('Username')
-#		passwd = st.sidebar.text_input('Password',type='password')
-#		if st.sidebar.checkbox('Login') :
-#			create_usertable()
-#			hashed_pswd = make_hashes(passwd)
-#			result = login_user(user,check_hashes(passwd,hashed_pswd))
-#			if result:
-#				st.success("Logged In as {}".format(user))
-
-				# Tasks For Only Logged In Users
-#				task = st.selectbox('Select Task',['Add Posts','Manage Blog','Profile'])
-#				if task == "Add Posts":
-#					st.subheader("Add Articles")
-					....
-# Security
-#passlib,hashlib,bcrypt,scrypt
-#import hashlib
-#def make_hashes(password):
-#	return hashlib.sha256(str.encode(password)).hexdigest()
-
-#def check_hashes(password,hashed_text):
-#	if make_hashes(password) == hashed_text:
-#		return hashed_text
-#	return False
-# DB Management
-#import sqlite3 
-#conn = sqlite3.connect('data.db')
-#c = conn.cursor()
-# DB  Functions
-#def create_usertable():
-#	c.execute('CREATE TABLE IF NOT EXISTS userstable(username TEXT,password TEXT)')
-
-
-#def add_userdata(username,password):
-#	c.execute('INSERT INTO userstable(username,password) VALUES (?,?)',(username,password))
-#	conn.commit()
-
-#def login_user(username,password):
-#	c.execute('SELECT * FROM userstable WHERE username =? AND password = ?',(username,password))
-#	data = c.fetchall()
-#	return data
-
-
-#def view_all_users():
-#	c.execute('SELECT * FROM userstable')
-#	data = c.fetchall()
-#	return data
-
-
-
-
 
 def main():
 	"""Simple Login App"""
@@ -129,22 +40,6 @@ def main():
 					st.dataframe(clean_db)
 			else:
 				st.warning("Incorrect Username/Password")
-
-
-
-
-
-	elif choice == "SignUp":
-		st.subheader("Create New Account")
-		new_user = st.text_input("Username")
-		new_password = st.text_input("Password",type='password')
-
-		if st.button("Signup"):
-			create_usertable()
-			add_userdata(new_user,make_hashes(new_password))
-			st.success("You have successfully created a valid Account")
-			st.info("Go to Login Menu to login")
-
 
 
 if __name__ == '__main__':
