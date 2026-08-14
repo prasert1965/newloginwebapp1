55ce0efd1adc021612ea0efbab0f4a3396d013e1
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-08-14 15:13:24 +0700

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -71,9 +71,9 @@ def main():
 				st.warning("Incorrect Username/Password")
 	
 	elif choice == "SignUp":
-       st.subheader("Create an Account")
-	new_user = st.text_input('Username')
-	new_passwd = st.text_input('Password',type='password')
+         st.subheader("Create an Account")
+	     new_user = st.text_input('Username')
+	     new_passwd = st.text_input('Password',type='password')
 	if st.button('SignUp'):
 		create_usertable()
 		add_userdata(new_user,make_hashes(new_passwd))
