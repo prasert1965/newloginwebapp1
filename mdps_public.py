c0e3908d2df310ce097240c64a270f532c0c24d5
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-08-14 14:34:59 +0700

    Refactor DB management and user authentication

--- mdps_public.py
+++ mdps_public.py
@@ -54,37 +54,37 @@ import pandas as pd
 					....
 # Security
 #passlib,hashlib,bcrypt,scrypt
-import hashlib
-def make_hashes(password):
-	return hashlib.sha256(str.encode(password)).hexdigest()
+#import hashlib
+#def make_hashes(password):
+#	return hashlib.sha256(str.encode(password)).hexdigest()
 
-def check_hashes(password,hashed_text):
-	if make_hashes(password) == hashed_text:
-		return hashed_text
-	return False
+#def check_hashes(password,hashed_text):
+#	if make_hashes(password) == hashed_text:
+#		return hashed_text
+#	return False
 # DB Management
-import sqlite3 
-conn = sqlite3.connect('data.db')
-c = conn.cursor()
+#import sqlite3 
+#conn = sqlite3.connect('data.db')
+#c = conn.cursor()
 # DB  Functions
-def create_usertable():
-	c.execute('CREATE TABLE IF NOT EXISTS userstable(username TEXT,password TEXT)')
+#def create_usertable():
+#	c.execute('CREATE TABLE IF NOT EXISTS userstable(username TEXT,password TEXT)')
 
 
-def add_userdata(username,password):
-	c.execute('INSERT INTO userstable(username,password) VALUES (?,?)',(username,password))
-	conn.commit()
+#def add_userdata(username,password):
+#	c.execute('INSERT INTO userstable(username,password) VALUES (?,?)',(username,password))
+#	conn.commit()
 
-def login_user(username,password):
-	c.execute('SELECT * FROM userstable WHERE username =? AND password = ?',(username,password))
-	data = c.fetchall()
-	return data
+#def login_user(username,password):
+#	c.execute('SELECT * FROM userstable WHERE username =? AND password = ?',(username,password))
+#	data = c.fetchall()
+#	return data
 
 
-def view_all_users():
-	c.execute('SELECT * FROM userstable')
-	data = c.fetchall()
-	return data
+#def view_all_users():
+#	c.execute('SELECT * FROM userstable')
+#	data = c.fetchall()
+#	return data
 
 
 
