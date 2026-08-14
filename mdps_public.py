fb046a38467ee618b2e18db4636158fc8446ceaf
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-08-14 13:55:32 +0700

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -1,1 +1,17 @@
+# DB Management
+import sqlite3 
+conn = sqlite3.connect('data.db')
+c = conn.cursor()
+def create_usertable():
+	c.execute('CREATE TABLE IF NOT EXISTS userstable(username TEXT,password TEXT)')
+
+
+def add_userdata(username,password):
+	c.execute('INSERT INTO userstable(username,password) VALUES (?,?)',(username,password))
+	conn.commit()
+
+def login_user(username,password):
+	c.execute('SELECT * FROM userstable WHERE username =? AND password = ?',(username,password))
+	data = c.fetchall()
+	return data
 
