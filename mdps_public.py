3fa4bf1d4aebb22c54447618fef0e369af04f0a5
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-08-14 14:03:41 +0700

    Refactor DB management and add signup feature
    Refactor DB management code structure and add user signup functionality.

--- mdps_public.py
+++ mdps_public.py
@@ -15,3 +15,12 @@ def login_user(username,password):
 	data = c.fetchall()
 	return data
 
+elif choice == "SignUp":
+       st.subheader("Create an Account")
+	new_user = st.text_input('Username')
+	new_passwd = st.text_input('Password',type='password')
+	if st.button('SignUp'):
+		create_usertable()
+		add_userdata(new_user,make_hashes(new_passwd))
+		st.success("You have successfully created an account.Go to the Login Menu to login")
+		
