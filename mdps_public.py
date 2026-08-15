264b4ffd239c5d79d87155a1af3bd7d48041e160
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-08-15 15:30:30 +0700

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -71,36 +71,36 @@ def main():
 
 				result = login_user(username,check_hashes(password,hashed_pswd))
 			
-				if result:
-					st.success("Logged In as {}".format(username))
+			if result:
+				st.success("Logged In as {}".format(username))
 
-					# getting the input data from the user
-					col1, col2, col3, col4 = st.columns(4)
-					with col1:
-		   				AGE = st.text_input('AGE (years)')
-					with col2:
-		   				HCT = st.text_input('Hematocrit (%)')
-					with col3:
-		   				HGB = st.text_input('Hemaglobin (g/dl)')
-					with col4:
-		   				RBC = st.text_input('RBC count(10^6 cells/cumm')
-					with col1:
-		   				MCV = st.text_input('MCV (fl)')
-					with col2:
-		   				MCH = st.text_input('MCH (pg)')
-					with col3:
-		   				MCHC = st.text_input('MCHC (g/dl)')
-					with col4:
-		   				RDW = st.text_input('RDW (fl)')
-					# code for Prediction
-					diagnosis = ''
+				# getting the input data from the user
+				col1, col2, col3, col4 = st.columns(4)
+				with col1:
+		   			AGE = st.text_input('AGE (years)')
+				with col2:
+		   			HCT = st.text_input('Hematocrit (%)')
+				with col3:
+		   			HGB = st.text_input('Hemaglobin (g/dl)')
+				with col4:
+		   			RBC = st.text_input('RBC count(10^6 cells/cumm')
+				with col1:
+		   			MCV = st.text_input('MCV (fl)')
+				with col2:
+		   			MCH = st.text_input('MCH (pg)')
+				with col3:
+		   			MCHC = st.text_input('MCHC (g/dl)')
+				with col4:
+		   			RDW = st.text_input('RDW (fl)')
+				# code for Prediction
+				diagnosis = ''
     
-					# creating a button for Prediction 
+				# creating a button for Prediction 
         
-					if st.button('Prediction result Pls. Click'):        
-		   				diagnosis = EA_Alpha_thal_prediction([AGE, HCT, HGB, RBC, MCV, MCH, MCHC, RDW])               
+				if st.button('Prediction result Pls. Click'):        
+		   			diagnosis = EA_Alpha_thal_prediction([AGE, HCT, HGB, RBC, MCV, MCH, MCHC, RDW])               
 				
-					st.success(diagnosis)
+				st.success(diagnosis)
        
 				col1, col2, col3, = st.columns(3)
 				with col1:
