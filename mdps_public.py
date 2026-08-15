f83ad6d6622a279d690dafad6fe4785b5348d625
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-08-15 15:38:40 +0700

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -75,61 +75,61 @@ def main():
 				
 			st.success("Logged In as {}".format(username))
 
-				# getting the input data from the user
-				col1, col2, col3, col4 = st.columns(4)
-				with col1:
-		   			AGE = st.text_input('AGE (years)')
-				with col2:
-		   			HCT = st.text_input('Hematocrit (%)')
-				with col3:
-		   			HGB = st.text_input('Hemaglobin (g/dl)')
-				with col4:
-		   			RBC = st.text_input('RBC count(10^6 cells/cumm')
-				with col1:
-		   			MCV = st.text_input('MCV (fl)')
-				with col2:
-		   			MCH = st.text_input('MCH (pg)')
-				with col3:
-		   			MCHC = st.text_input('MCHC (g/dl)')
-				with col4:
-		   			RDW = st.text_input('RDW (fl)')
-				# code for Prediction
-				diagnosis = ''
+			# getting the input data from the user
+			col1, col2, col3, col4 = st.columns(4)
+			with col1:
+		   		AGE = st.text_input('AGE (years)')
+			with col2:
+		   		HCT = st.text_input('Hematocrit (%)')
+			with col3:
+		   		HGB = st.text_input('Hemaglobin (g/dl)')
+			with col4:
+		   		RBC = st.text_input('RBC count(10^6 cells/cumm')
+			with col1:
+		   		MCV = st.text_input('MCV (fl)')
+			with col2:
+		   		MCH = st.text_input('MCH (pg)')
+			with col3:
+		   		MCHC = st.text_input('MCHC (g/dl)')
+			with col4:
+		   		RDW = st.text_input('RDW (fl)')
+			# code for Prediction
+			diagnosis = ''
     
-				# creating a button for Prediction 
+			# creating a button for Prediction 
         
-				if st.button('Prediction result Pls. Click'):        
-		   			diagnosis = EA_Alpha_thal_prediction([AGE, HCT, HGB, RBC, MCV, MCH, MCHC, RDW])               
+			if st.button('Prediction result Pls. Click'):        
+		   		diagnosis = EA_Alpha_thal_prediction([AGE, HCT, HGB, RBC, MCV, MCH, MCHC, RDW])               
 				
-				st.success(diagnosis)
+			st.success(diagnosis)
        
-				col1, col2, col3, = st.columns(3)
-				with col1:
-		  			st.write('Predicted by ..Phrae ADA ML.. ') 
-				with col2:
-		   			st.write('Reported by ............................ ')   
-				with col3:
-		   			st.write('Approved by ............................ ')
+			col1, col2, col3, = st.columns(3)
+			with col1:
+	  			st.write('Predicted by ..Phrae ADA ML.. ') 
+			with col2:
+	   			st.write('Reported by ............................ ')   
+			with col3:
+		   		st.write('Approved by ............................ ')
 		   	
-				st.write(f"**Date Prediction:** {current_time_str}")	
+			st.write(f"**Date Prediction:** {current_time_str}")	
 
-				#st.title("รายงานผลการทำนาย")
-				# สร้างปุ่มพิมพ์หน้าเว็บ
-				components.html("""
-    			<button onclick="window.parent.print()" style="
-        			background-color: #4CAF50;
-        			color: white;
-        			padding: 10px 24px;
-        			border: none;
-        			border-radius: 4px;
-        			cursor: pointer;
-        			font-size: 16px;">
-        			🖨️ พิมพ์รายงาน (Print / Save as PDF)
-    			</button>
-				""", height=60)
+			#st.title("รายงานผลการทำนาย")
+			# สร้างปุ่มพิมพ์หน้าเว็บ
+			components.html("""
+    		<button onclick="window.parent.print()" style="
+        		background-color: #4CAF50;
+        		color: white;
+        		padding: 10px 24px;
+        		border: none;
+        		border-radius: 4px;
+        		cursor: pointer;
+        		font-size: 16px;">
+        		🖨️ พิมพ์รายงาน (Print / Save as PDF)
+    		</button>
+			""", height=60)
 		
-			else:
- 		  		 st.warning("Incorrect Username/Password")
+		else:
+ 	 		 st.warning("Incorrect Username/Password")
 
 		
 	
