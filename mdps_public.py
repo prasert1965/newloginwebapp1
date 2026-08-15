9b6f4064d6a04dbe6c8c2ced42cb94f3b6015d4a
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-08-15 09:55:34 +0700

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -84,7 +84,45 @@ def main():
       			return 'This person is alpha thalassemia carrier'
      		else:
        			return 'This person is not alpha thalassemia carrier'
-				
+			# giving a title  
+    		st.title('Web for prediction Alpha Thalassemia carrier')   
+    
+    		# getting the input data from the user
+    		col1, col2, col3, col4 = st.columns(4)
+    		with col1:
+         		AGE = st.text_input('AGE (years)')
+    		with col2:
+         		HCT = st.text_input('Hematocrit (%)')
+    		with col3:
+         		HGB = st.text_input('Hemaglobin (g/dl)')
+    		with col4:
+         		RBC = st.text_input('RBC count(10^6 cells/cumm')
+    		with col1:
+         		MCV = st.text_input('MCV (fl)')
+    		with col2:
+         		MCH = st.text_input('MCH (pg)')
+    		with col3:
+         		MCHC = st.text_input('MCHC (g/dl)')
+    		with col4:
+         		RDW = st.text_input('RDW (fl)')
+       
+		    # code for Prediction
+    		diagnosis = ''
+    
+		    # creating a button for Prediction 
+        
+    		if st.button('Prediction result Pls. Click'):        
+       			diagnosis = EA_Alpha_thal_prediction([AGE, HCT, HGB, RBC, MCV, MCH, MCHC, RDW])               
+	        st.success(diagnosis)
+       
+    		col1, col2, col3, = st.columns(3)
+    		with col1:
+          	st.write('Predicted by ..Phrae ADA ML.. ') 
+    		with col2:
+          	st.write('Reported by ............................ ')   
+    		with col3:
+          	st.write('Approved by ............................ ')
+    		st.write(f"**Date Prediction:** {current_time_str}")	
 				#task = st.selectbox("Task",["Add Post","Analytics","Profiles"])
 				#if task == "Add Post":
 				#	st.subheader("Add Your Post")
