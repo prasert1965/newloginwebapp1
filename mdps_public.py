d30dc9455b17521a71b4af100f6b563938adb6ba
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-08-15 14:44:42 +0700

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -77,6 +77,25 @@ def main():
 		else:
  			st.warning("Incorrect Username/Password")
 
+		# getting the input data from the user
+		col1, col2, col3, col4 = st.columns(4)
+		with col1:
+		   	AGE = st.text_input('AGE (years)')
+		with col2:
+		   	HCT = st.text_input('Hematocrit (%)')
+		with col3:
+		   	HGB = st.text_input('Hemaglobin (g/dl)')
+		with col4:
+		   	RBC = st.text_input('RBC count(10^6 cells/cumm')
+		with col1:
+		   	MCV = st.text_input('MCV (fl)')
+		with col2:
+		   	MCH = st.text_input('MCH (pg)')
+		with col3:
+		   	MCHC = st.text_input('MCHC (g/dl)')
+		with col4:
+		   	RDW = st.text_input('RDW (fl)')
+	
 	elif choice == "SignUp":
 		st.subheader("Create an Account")
 
@@ -108,24 +127,6 @@ def EA_Alpha_thal_prediction(input_data):
 # giving a title  
 #	st.title('Web for prediction Alpha Thalassemia carrier')   
     
-# getting the input data from the user
-col1, col2, col3, col4 = st.columns(4)
-with col1:
-   	AGE = st.text_input('AGE (years)')
-with col2:
-   	HCT = st.text_input('Hematocrit (%)')
-with col3:
-   	HGB = st.text_input('Hemaglobin (g/dl)')
-with col4:
-   	RBC = st.text_input('RBC count(10^6 cells/cumm')
-with col1:
-   	MCV = st.text_input('MCV (fl)')
-with col2:
-   	MCH = st.text_input('MCH (pg)')
-with col3:
-   	MCHC = st.text_input('MCHC (g/dl)')
-with col4:
-   	RDW = st.text_input('RDW (fl)')
        
 # code for Prediction
 diagnosis = ''
