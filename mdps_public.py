7a5057f2dfa23138f6cfb7736c9f2ff91572e5a7
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-08-15 13:12:06 +0700

    Refactor DB management and login functionality

--- mdps_public.py
+++ mdps_public.py
@@ -99,22 +99,22 @@ else:
     
 # getting the input data from the user
 col1, col2, col3, col4 = st.columns(4)
-	with col1:
-    	AGE = st.text_input('AGE (years)')
-	with col2:
-    	HCT = st.text_input('Hematocrit (%)')
-	with col3:
-    	HGB = st.text_input('Hemaglobin (g/dl)')
-	with col4:
-    	RBC = st.text_input('RBC count(10^6 cells/cumm')
-	with col1:
-    	MCV = st.text_input('MCV (fl)')
-	with col2:
-    	MCH = st.text_input('MCH (pg)')
-	with col3:
-    	MCHC = st.text_input('MCHC (g/dl)')
-	with col4:
-    	RDW = st.text_input('RDW (fl)')
+with col1:
+   	AGE = st.text_input('AGE (years)')
+with col2:
+   	HCT = st.text_input('Hematocrit (%)')
+with col3:
+   	HGB = st.text_input('Hemaglobin (g/dl)')
+with col4:
+   	RBC = st.text_input('RBC count(10^6 cells/cumm')
+with col1:
+   	MCV = st.text_input('MCV (fl)')
+with col2:
+   	MCH = st.text_input('MCH (pg)')
+with col3:
+   	MCHC = st.text_input('MCHC (g/dl)')
+with col4:
+   	RDW = st.text_input('RDW (fl)')
        
 # code for Prediction
     diagnosis = ''
