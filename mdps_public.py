cfbbd70b897eb938e8f5191c54debd9b96a5c69a
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-08-15 15:01:52 +0700

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -102,14 +102,14 @@ def main():
 				
 				st.success(diagnosis)
        
-		col1, col2, col3, = st.columns(3)
-		with col1:
-		  	st.write('Predicted by ..Phrae ADA ML.. ') 
-		with col2:
-		   	st.write('Reported by ............................ ')   
-		with col3:
-		   	st.write('Approved by ............................ ')
-		   	st.write(f"**Date Prediction:** {current_time_str}")	
+			col1, col2, col3, = st.columns(3)
+			with col1:
+		  		st.write('Predicted by ..Phrae ADA ML.. ') 
+			with col2:
+		   		st.write('Reported by ............................ ')   
+			with col3:
+		   		st.write('Approved by ............................ ')
+		   		st.write(f"**Date Prediction:** {current_time_str}")	
 		else:
  			st.warning("Incorrect Username/Password")
 
