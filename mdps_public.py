45d136dec391614aab56cb3cedf304f56a86c143
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-08-15 10:06:11 +0700

    Refactor DB management and login functionality

--- mdps_public.py
+++ mdps_public.py
@@ -68,61 +68,62 @@ def main():
 			if result:
 
 					st.success("Logged In as {}".format(username))
-					def EA_Alpha_thal_prediction(input_data):
+					
+def EA_Alpha_thal_prediction(input_data):
 
-    					# changing the input_data to numpy array
-     					input_data_as_numpy_array = np.asarray(input_data)
+# changing the input_data to numpy array
+input_data_as_numpy_array = np.asarray(input_data)
 
-	    				# reshape the array as we are predicting for one instance
-    	 				input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+# reshape the array as we are predicting for one instance
+input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
 
-     					prediction = loaded_model.predict(input_data_reshaped)
-     					print(input_data_as_numpy_array)
-     					print(prediction)
+prediction = loaded_model.predict(input_data_reshaped)
+print(input_data_as_numpy_array)
+print(prediction)
 
-     				if (prediction[0] == 0):
-      					return 'This person is alpha thalassemia carrier'
-     				else:
-       					return 'This person is not alpha thalassemia carrier'
-					# giving a title  
-    				st.title('Web for prediction Alpha Thalassemia carrier')   
+if (prediction[0] == 0):
+    return 'This person is alpha thalassemia carrier'
+else:
+    return 'This person is not alpha thalassemia carrier'
+# giving a title  
+st.title('Web for prediction Alpha Thalassemia carrier')   
     
-    				# getting the input data from the user
-    				col1, col2, col3, col4 = st.columns(4)
-    				with col1:
-         				AGE = st.text_input('AGE (years)')
-    				with col2:
-         				HCT = st.text_input('Hematocrit (%)')
-    				with col3:
-         				HGB = st.text_input('Hemaglobin (g/dl)')
-    				with col4:
-         				RBC = st.text_input('RBC count(10^6 cells/cumm')
-    				with col1:
-         				MCV = st.text_input('MCV (fl)')
-    				with col2:
-         				MCH = st.text_input('MCH (pg)')
-    				with col3:
-         				MCHC = st.text_input('MCHC (g/dl)')
-    				with col4:
-         				RDW = st.text_input('RDW (fl)')
+# getting the input data from the user
+col1, col2, col3, col4 = st.columns(4)
+with col1:
+    AGE = st.text_input('AGE (years)')
+with col2:
+    HCT = st.text_input('Hematocrit (%)')
+with col3:
+    HGB = st.text_input('Hemaglobin (g/dl)')
+with col4:
+    RBC = st.text_input('RBC count(10^6 cells/cumm')
+with col1:
+    MCV = st.text_input('MCV (fl)')
+with col2:
+    MCH = st.text_input('MCH (pg)')
+with col3:
+    MCHC = st.text_input('MCHC (g/dl)')
+with col4:
+    RDW = st.text_input('RDW (fl)')
        
-		    		# code for Prediction
-    				diagnosis = ''
+# code for Prediction
+    diagnosis = ''
     
-				    # creating a button for Prediction 
+# creating a button for Prediction 
         
-		    		if st.button('Prediction result Pls. Click'):        
-       					diagnosis = EA_Alpha_thal_prediction([AGE, HCT, HGB, RBC, MCV, MCH, MCHC, RDW])               
-	        		st.success(diagnosis)
+if st.button('Prediction result Pls. Click'):        
+    diagnosis = EA_Alpha_thal_prediction([AGE, HCT, HGB, RBC, MCV, MCH, MCHC, RDW])               
+st.success(diagnosis)
        
-    				col1, col2, col3, = st.columns(3)
-    				with col1:
-          				st.write('Predicted by ..Phrae ADA ML.. ') 
-    				with col2:
-          				st.write('Reported by ............................ ')   
-    				with col3:
-          				st.write('Approved by ............................ ')
-    					st.write(f"**Date Prediction:** {current_time_str}")	
+col1, col2, col3, = st.columns(3)
+with col1:
+    st.write('Predicted by ..Phrae ADA ML.. ') 
+with col2:
+    st.write('Reported by ............................ ')   
+with col3:
+    st.write('Approved by ............................ ')
+    st.write(f"**Date Prediction:** {current_time_str}")	
 				#task = st.selectbox("Task",["Add Post","Analytics","Profiles"])
 				#if task == "Add Post":
 				#	st.subheader("Add Your Post")
