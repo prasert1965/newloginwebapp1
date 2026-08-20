36c9493e3c48cef620ef302d59df2ed063835cc7
prasert1965 <160126067+prasert1965@users.noreply.github.com>
2026-08-20 18:57:10 +0700

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -68,8 +68,7 @@ def main():
 	if choice == "Home":
 		st.subheader("This is Web app for prediction alpha thalassemia carriers")
 		st.write('In 2024, From Phrae Adaboost model on Dataset3 demonstrated acc 97% sen 100% spec 95% AUC 0.974. In 2026, A performance evaluation of the adaboost model in the Khon Kaen population showed acc 63% Sen 93.4% Spec 57.4% PPV 30.9% NPV 97.7% AUC 0.754.')
-        st.markdown(hide_github_icon, unsafe_allow_html=True)
-	
+        	
 	elif choice == "Login":
 		st.subheader("Login Section")
 		
@@ -155,6 +154,7 @@ def main():
 		   create_usertable()
 		   add_userdata(new_user,make_hashes(new_passwd))
 		   st.success("You have successfully created an account.Go to the Login Menu to login")
+	st.markdown(hide_github_icon, unsafe_allow_html=True)
 
 def EA_Alpha_thal_prediction(input_data):
 
